f6bd9df7c1a9a0360a7bff353a127bb085eb682f
Pablo F. Diaz - Developer <pfd1795@gmail.com>
2025-07-11 16:12:57 -0300

    actualizar
    actualizar

--- __test__/primerTest.test.ts
+++ __test__/primerTest.test.ts
@@ -1,6 +1,6 @@
 describe('Primer test para validar el workflow de GitHub', () => {
     test('Debe confirmar que la suma básica se evalúa correctamente', () => {
-        const resultado = 4 + 0;
+        const resultado = 4 + 4;
         expect(resultado).toBe(4); // Verifica que 2 + 2 efectivamente sea igual a 4
     });
 });
